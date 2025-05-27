0e848dd21b48f65b4b331eb14e6babe008508968
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-05-27 16:09:49 -0300

    Realizo mi archivo Calculadora del punto 1

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,30 @@
+namespace EspacioCalculadora;
+
+    class Calculadora
+    {
+        double resultado;
+
+        public double Resultado { get => resultado;}
+
+        public void Sumar(double termino)
+        {
+            resultado += termino;
+        }
+        public void Restar(double termino)
+        {
+            resultado -= termino;
+        }
+        public void Multiplicar(double termino)
+        {
+            resultado *= termino;
+        }
+        public void Dividir(double termino)
+        {
+            if (termino != 0)
+            {
+                resultado = resultado / termino;    
+            }
+            
+        }
+    }
+
