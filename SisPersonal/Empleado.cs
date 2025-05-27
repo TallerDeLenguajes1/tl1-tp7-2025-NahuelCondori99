3c34612574c13ff34f3bd51174fbe90351c24159
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-05-27 16:34:33 -0300

    Creo mi carpeta SisPersonal y realizo mi clase para comenzar a desarrollar el punto 2

---
+++ SisPersonal/Empleado.cs
@@ -0,0 +1,27 @@
+namespace InfoEmpleados;
+
+public class InfoEmpleados
+{
+    private string nombre;
+    private string apellido;
+    private char estadoCivil;
+    private double sueldoBasico;
+    private DateTime FechNac;
+    private DateTime FechIngre;
+    
+    private enum Cargos
+    {
+        Auxiliar,
+        Administrativo,
+        Ingeniero,
+        Especialista,
+        Investigador
+    }
+
+    public string Nombre { get => nombre; }
+    public string Apellido { get => apellido; }
+    public char EstadoCivil { get => estadoCivil; }
+    public double SueldoBasico { get => sueldoBasico; }
+    public DateTime FechNac1 { get => FechNac;}
+    public DateTime FechIngre1 { get => FechIngre;}
+}
