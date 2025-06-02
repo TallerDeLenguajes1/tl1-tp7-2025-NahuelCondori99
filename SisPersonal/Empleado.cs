16be9ba898ff32820a7640aa1573fa6f75a06da1
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-06-02 20:41:03 -0300

    Realice correcciones a mi archivo Empleado.cs ya que habia entendido mal el tema e hice mal el desarrollo

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,14 +1,8 @@
 namespace InfoEmpleados;
 
-public class InfoEmpleados
-{
-    private string nombre;
-    private string apellido;
-    private char estadoCivil;
-    private double sueldoBasico;
-    private DateTime FechNac;
-    private DateTime FechIngre;
 
+class InfoEmpleados
+{
     private enum Cargos
     {
         Auxiliar,
@@ -17,6 +11,26 @@ public class InfoEmpleados
         Especialista,
         Investigador
     }
+    private string nombre;
+    private string apellido;
+    private char estadoCivil;
+    private double sueldoBasico;
+    private DateTime FechNac;
+    private DateTime FechIngre;
+    private Cargos cargo;
+
+    public InfoEmpleados(string nombre, string apellido, char estadoCivil, double sueldoBasico, DateTime FechNac, DateTime FechIngre)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.estadoCivil = estadoCivil;
+        this.sueldoBasico = sueldoBasico;
+        this.FechNac = FechNac;
+        this.FechIngre = FechIngre;
+        this.Cargo = Cargo;
+    }
+
+
 
     public string Nombre { get => nombre; }
     public string Apellido { get => apellido; }
@@ -24,31 +38,16 @@ public class InfoEmpleados
     public double SueldoBasico { get => sueldoBasico; }
     public DateTime FechNac1 { get => FechNac; }
     public DateTime FechIngre1 { get => FechIngre; }
+    private Cargos Cargo { get => cargo; }
 
-
-    public void NomEmpleado(string nombre)
+    public void mostrarDatos()
     {
-        nombre = Console.ReadLine();
-    }
-    public void ApEmpleado(string apellido)
-    {
-        apellido = Console.ReadLine();
-    }
-    public void FechNaci(DateTime fechaNac)
-    {
-
-    }
-    public void Estado(char estadoCivil)
-    {
-        
-    }
-    public void Sueldo(double sueldoB)
-    {
-
-    }
-
-    public void FechIn(DateTime fechaIn)
-    {
-
+        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Apellido: " + apellido);
+        Console.WriteLine("Estado Civil: " + estadoCivil);
+        Console.WriteLine("Sueldo Básico: " + sueldoBasico);
+        Console.WriteLine("Fecha de Nacimiento: " + FechNac.ToShortDateString());
+        Console.WriteLine("Fecha de Ingreso: " + FechIngre.ToShortDateString());
+        Console.WriteLine("Cargo: " + cargo);
     }
 }
