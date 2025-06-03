3df15711dc7e1d64a79524af188372f63dd90b04
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-06-03 00:20:05 -0300

    Modificacion ejercicio 2

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace InfoEmpleados;
 
 
@@ -15,37 +17,78 @@ class Empleados
     private string apellido;
     private char estadoCivil;
     private double sueldoBasico;
-    private DateTime FechNac;
-    private DateTime FechIngre;
+    private DateTime fechNac;
+    private DateTime fechIngre;
     private Cargos cargo;
 
-    public Empleados(string nombre, string apellido, char estadoCivil, double sueldoBasico, DateTime FechNac, DateTime FechIngre, Cargos cargo)
+    public Empleados(string nombre, string apellido, char estadoCivil, double sueldoBasico, DateTime fechNac, DateTime fechIngre, Cargos cargo)
     {
         this.nombre = nombre;
         this.apellido = apellido;
         this.estadoCivil = estadoCivil;
         this.sueldoBasico = sueldoBasico;
-        this.FechNac = FechNac;
-        this.FechIngre = FechIngre;
+        this.fechNac = fechNac;
+        this.fechIngre = fechIngre;
         this.cargo = cargo;
     }
+
 
     public string Nombre { get => nombre; }
     public string Apellido { get => apellido; }
     public char EstadoCivil { get => estadoCivil; }
     public double SueldoBasico { get => sueldoBasico; }
-    public DateTime FechNac1 { get => FechNac; }
-    public DateTime FechIngre1 { get => FechIngre; }
-    private Cargos Cargo { get => cargo; }
+    public DateTime FechNac { get => fechNac; }
+    public DateTime FechIngre { get => fechIngre; }
+    public Cargos Cargo { get => cargo; }
 
-    public void mostrarDatos()
+
+    //EJERCICIO 2
+    public int edad => DateTime.Now.Year - fechNac.Year;
+    public int antiguedad => DateTime.Now.Year - fechIngre.Year;
+    public int jubilacion => 65 - edad;
+
+    public double CalcSalario()
+    {
+        double adicional = 0;
+
+        if (antiguedad < 21)
+        {
+            adicional = sueldoBasico * (antiguedad * 0.01);
+        }
+        else
+        {
+            adicional = sueldoBasico * 0.25;
+        }
+
+        if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+        {
+            adicional = adicional * 1.5;
+        }
+
+        if (char.ToUpper(estadoCivil) == 'C')
+        {
+            adicional += 150000;
+        }
+
+        double salarioTotal = sueldoBasico + adicional;
+
+        return salarioTotal;
+    }
+
+    public void MostrarDatos()
     {
         Console.WriteLine("Nombre: " + nombre);
         Console.WriteLine("Apellido: " + apellido);
         Console.WriteLine("Estado Civil: " + estadoCivil);
-        Console.WriteLine("Sueldo Básico: " + sueldoBasico);
-        Console.WriteLine("Fecha de Nacimiento: " + FechNac.ToShortDateString());
-        Console.WriteLine("Fecha de Ingreso: " + FechIngre.ToShortDateString());
+        Console.WriteLine("Sueldo Básico: $ " + sueldoBasico);
+        Console.WriteLine("Fecha de Nacimiento: " + fechNac.ToShortDateString());
+        Console.WriteLine("Fecha de Ingreso: " + fechIngre.ToShortDateString());
         Console.WriteLine("Cargo: " + cargo);
+        Console.WriteLine("Edad del empleado: " + edad + " anios");
+        Console.WriteLine("Salario del empleado: $" + CalcSalario());
+        Console.WriteLine("Antiguedad del empleado en la empresa: " + antiguedad + " anios");
+        Console.WriteLine("Anios restantes para jubilarse: " + jubilacion);
+        Console.WriteLine("---------------------------------");
     }
+    
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,16 +1,32 @@
 ﻿using System.Globalization;
 using InfoEmpleados;
 
-Empleados miEmpleado = new Empleados
-(
-    "Nahuel",
-    "Condori",
-    's',
-    750000,
-    new DateTime(1999, 07, 15),
-    new DateTime(2021, 03, 16),
-    cargo: Empleados.Cargos.Ingeniero
+Empleados[] miEmpleado = new Empleados[3];
 
-);
+    miEmpleado[0] = new Empleados("Elias", "Condori", 'C', 650000, new DateTime(1999, 7, 15), new DateTime(2010, 3, 15), Empleados.Cargos.Ingeniero);
 
-miEmpleado.mostrarDatos();
+    miEmpleado[1] = new Empleados("Santos", "Condori", 'C', 850000, new DateTime(2000, 10, 1), new DateTime(2022, 4, 15), Empleados.Cargos.Administrativo);
+
+    miEmpleado[2] = new Empleados("Nahuel", "Condori", 'S', 750000, new DateTime(2001, 7, 16), new DateTime(2019, 7, 15), Empleados.Cargos.Administrativo);
+
+double montoSalarioTotal = 0;
+
+for (int i = 0; i < miEmpleado.Length; i++)
+{
+    miEmpleado[i].MostrarDatos();
+
+    montoSalarioTotal += miEmpleado[i].CalcSalario();
+}
+Console.WriteLine("El monto total en conceptos de salarios es: $" + montoSalarioTotal);
+Console.WriteLine("---------------------------------");
+Empleados jubiMaxima = miEmpleado[0];
+
+for (int i = 0; i < miEmpleado.Length; i++)
+{
+    if (miEmpleado[i].jubilacion < jubiMaxima.jubilacion)
+    {
+        jubiMaxima = miEmpleado[i];
+    }
+}
+Console.WriteLine("El empleado mas proximo a jubilarse es: ");
+jubiMaxima.MostrarDatos();
