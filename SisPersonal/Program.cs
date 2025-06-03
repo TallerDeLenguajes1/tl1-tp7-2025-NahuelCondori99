33e0b7aa0b89069de1ba3cec8ab11e70a6ffc988
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-06-02 21:44:14 -0300

    Rellene los datos de forma manual para poder probar si esta todo en orden

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using InfoEmpleados;
+
+Empleados miEmpleado = new Empleados
+(
+    "Nahuel",
+    "Condori",
+    's',
+    750000,
+    new DateTime(1999, 07, 15),
+    new DateTime(2021, 03, 16),
+    cargo: Empleados.Cargos.Ingeniero
+
+);
+
+miEmpleado.mostrarDatos();
